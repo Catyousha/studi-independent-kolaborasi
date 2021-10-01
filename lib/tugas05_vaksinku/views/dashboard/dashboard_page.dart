4d6e35bfc7b0085286952f32a35ad0faf56a3648
abdulazizpermana <abdulazizpermana106@gmail.com>
2021-10-01 21:37:10 +0700

    membuat appbar dan juga banner

--- lib/tugas05_vaksinku/views/dashboard/dashboard_page.dart
+++ lib/tugas05_vaksinku/views/dashboard/dashboard_page.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class DashboardPage extends StatelessWidget {
@@ -6,8 +7,51 @@ class DashboardPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: Container(),
+      body: SafeArea(
+        child: Column(
+          children: [
+            Container(
+              padding: EdgeInsets.all(15.0),
+              child: Center(
+                child: Column(
+                  children: [
+                    Image.asset(
+                      'assets/images/VaksinKu.png',
+                      height: 30,
+                      width: 150,
+                    ),
+                    SizedBox(
+                      height: 30,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        Text(
+                          'Selamat Siang!\nAmanda',
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold, fontSize: 18),
+                        ),
+                        Image.asset(
+                          'assets/images/icon_notif.png',
+                          height: 28,
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Image.asset('assets/images/banner.png'),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Image.asset('assets/images/bullet.png'),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
